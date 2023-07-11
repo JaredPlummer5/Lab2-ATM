e1ac6c6ae13003d5ef75244ef3ea40ffde93bda4
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-07-11 12:37:42 -0500

    Done

--- Lab2-ATM-Test/UnitTest1.cs
+++ Lab2-ATM-Test/UnitTest1.cs
@@ -1,10 +1,38 @@
-﻿namespace Lab2_ATM_Test;
+﻿using Lab2;
+namespace Lab2_ATM_Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("12")]
+    [InlineData("22")]
+    [InlineData("15")]
+    [InlineData("2000")]
+    [InlineData("1000")]
+    public void ViewBalanceTest(string balance)
     {
+        var deciamalBalance = Convert.ToDecimal(balance);
+       Assert.Equal(deciamalBalance, Program.ViewBalanceTest(deciamalBalance));
+    }
 
+    [Theory]
+    [InlineData("52", "26")]
+    [InlineData("2", "34")]
+    [InlineData("65", "75")]
+    public void DepositT(string balance, string deposit)
+    {
+        var deciamalBalance = Convert.ToDecimal(balance);
+        var decimalDeposit = Convert.ToDecimal(deposit);
+        Assert.Equal(deciamalBalance + decimalDeposit, Program.DepositTest(deciamalBalance, decimalDeposit));
+    }
+
+    [Theory]
+    [InlineData("75", "5")]
+    [InlineData("100", "55")]
+    public void WithdrawalTest(string balance, string withdrawal)
+    {
+        var deciamalBalance = Convert.ToDecimal(balance);
+        var deciamalWithdrawal = Convert.ToDecimal(withdrawal);
+        Assert.Equal(deciamalBalance - deciamalWithdrawal, Program.WithdrawTest(deciamalBalance, deciamalWithdrawal));
     }
 }
--- Lab2-ATM/Program.cs
+++ Lab2-ATM/Program.cs
@@ -6,8 +6,152 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadLine();
+            UserInterface();
         }
+
+        public static void UserInterface()
+        {
+            Console.WriteLine("Welcome!");
+
+            // Console.WriteLine("Current Balance: " + balance);
+            decimal balance = 100.05m;
+            int play = 0;
+            do
+            {
+                Console.WriteLine("Press 1 to see your Balance");
+                Console.WriteLine("Press 2 to Withdraw money");
+                Console.WriteLine("Press 3 to Deposit money");
+                int action = Convert.ToInt32(Console.ReadLine());
+
+
+                if (action == 1)
+                {
+                    balance = ViewBalance(balance);
+                    Console.WriteLine($"Current Balance: ${balance}");
+                }
+                else if (action == 2)
+                {
+                    balance = Withdraw(balance);
+                    Console.WriteLine($"Current Balance: ${balance}");
+
+
+                }
+                else if (action == 3)
+                {
+                    balance = Deposit(balance);
+                    Console.WriteLine($"Current Balance: ${balance}");
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter a vaild option");
+                }
+
+               
+            } while (play == 0);
+        }
+
+
+
+
+
+        public static decimal ViewBalance(decimal balance)
+        {
+            return balance;
+
+        }
+
+        ///////////////////////////////////////////////// TEST FOR VIEWBALANCE ///////////////////////////////////////
+        public static decimal ViewBalanceTest(decimal balance)
+        {
+            return balance;
+
+        }
+
+        ///////////////////////////////////////////////// TEST FOR VIEWBALANCE ///////////////////////////////////////
+
+        public static decimal Withdraw(decimal balance)
+
+        {
+            Console.Write("Select an amount: ");
+            decimal withdrawal = Convert.ToInt32(Console.ReadLine());
+
+            if (balance > 0)
+            {
+                balance -= withdrawal;
+            }
+            else
+            {
+                balance = 0;
+            }
+            return balance;
+        }
+
+
+        public static decimal WithdrawTest(decimal balance, decimal withdrawal)
+        {
+            Console.Write("Select an amount: ");
+
+
+            if (balance > 0 && (withdrawal > 0 || withdrawal >= balance))
+            {
+                balance -= withdrawal;
+            }
+            else
+            {
+                balance = 0;
+            }
+            return balance;
+        }
+
+
+        public static decimal Deposit(decimal balance)
+        {
+
+            Console.Write("Select an amount: ");
+            decimal deposit = Convert.ToInt32(Console.ReadLine());
+            while (deposit < 0)
+            {
+                Console.Write("Select an amount: ");
+                deposit = Convert.ToInt32(Console.ReadLine());
+            }
+            if (deposit > 0)
+            {
+                balance += deposit;
+
+            }
+            return balance;
+
+
+        }
+
+
+
+
+        public static decimal DepositTest(decimal balance, decimal deposit)
+        {
+            Console.Write("Select an amount: ");
+
+            while (deposit < 0)
+            {
+                Console.Write("Select an amount: ");
+                deposit = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (deposit > 0)
+            {
+                balance += deposit;
+
+            }
+            else
+            {
+                throw new Exception("invalid transaction");
+
+
+            }
+            return balance;
+        }
+
     }
 }
+
